2dbe6592d3e7d71de79453d0cd546d2b2893d9d4
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-11-25 13:34:12 -0700

    feat: create rag mcp server

---
+++ main.py
@@ -0,0 +1,113 @@
+import sys
+from collections.abc import Generator
+from pathlib import Path
+
+from langchain_core.documents import Document
+from langchain_core.embeddings import Embeddings
+from langchain_core.vectorstores import VectorStore
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_text_splitters import (
+    MarkdownHeaderTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
+from mcp.server.fastmcp import FastMCP
+from pydantic import BaseModel
+
+
+class RagResponse(BaseModel):
+    source: str
+    content: str
+
+
+class RAG:
+    def __init__(
+        self,
+        vector_store: VectorStore,
+        embeddings_model: Embeddings,
+        directory: Path,
+    ):
+        self.vector_store = vector_store
+        self.embeddings_model = embeddings_model
+        self.directory = directory
+
+        self._md_splitter = MarkdownHeaderTextSplitter(
+            headers_to_split_on=[
+                ("##", "Header 2"),
+                ("###", "Header 3"),
+                ("####", "Header 4"),
+                ("#####", "Header 5"),
+            ],
+            strip_headers=False,
+        )
+        self._recursive_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=2000,
+            chunk_overlap=50,
+            separators=["\n\n", "\n", " ", ""],
+        )
+
+    def _iterate_paths(self, directory: Path) -> Generator[str, None, None]:
+        for pth in directory.iterdir():
+            if pth.is_file():
+                yield pth.read_text("utf-8")
+                continue
+            yield from self._iterate_paths(pth)
+
+    def _split_text(self, file: str) -> list[Document]:
+        md_docs = self._md_splitter.split_text(file)
+        return self._recursive_splitter.split_documents(md_docs)
+
+    def ingest(self) -> None:
+        for file in self._iterate_paths(self.directory):
+            self.vector_store.add_documents(
+                self._split_text(file), metadata={"filename": file}
+            )
+
+    def query(self, query: str) -> list[RagResponse]:
+        """Retrieve information to help answer a query."""
+        docs = self.vector_store.similarity_search(query, k=2)
+        return [
+            RagResponse(
+                source=doc.metadata["filename"], content=doc.page_content
+            )
+            for doc in docs
+        ]
+
+
+def start_store(directory: Path) -> RAG:
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/gemini-embedding-001", task_type="RETRIEVAL_DOCUMENT"
+    )
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=directory.name,
+        connection="postgresql+psycopg://...",
+    )
+    return RAG(store, embeddings, directory)
+
+
+def run_mcp(rag: RAG) -> None:
+    mcp = FastMCP()
+
+    @mcp.tool()
+    def query(query: str) -> list[RagResponse]:
+        return rag.query(query)
+
+    mcp.run(transport="stdio")
+
+
+def main():
+    directory = Path(sys.argv[1])
+    rag = start_store(directory)
+    if len(sys.argv) < 2 or sys.argv[2] not in ["mcp", "ingest"]:
+        print("Must specify `ingest` or `mcp`.")
+        sys.exit(1)
+
+    if sys.argv[2] == "ingest":
+        rag.ingest()
+    elif sys.argv[2] == "mcp":
+        run_mcp(rag)
+
+
+if __name__ == "__main__":
+    main()
